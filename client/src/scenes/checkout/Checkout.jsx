33cebc204c22fa802ecf5311b6c691163a2599fe
Brian Nguyen <nguyen.brian70@gmail.com>
2025-02-11 15:13:02 -0500

    delete test

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -3,29 +3,6 @@ import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
 import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
-import { shades } from "../../theme";
-import Payment from "./Payment";
-import Shipping from "./Shipping";
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe(
-  "pk_test_51LgU7yConHioZHhlAcZdfDAnV9643a7N1CMpxlKtzI1AUWLsRyrord79GYzZQ6m8RzVnVQaHsgbvN1qSpiDegoPi006QkO0Mlc"
-);
-
-const Checkout = () => {
-  const [activeStep, setActiveStep] = useState(0);
-  const cart = useSelector((state) => state.cart.cart);
-  const isFirstStep = activeStep === 0;
-  const isSecondStep = activeStep === 1;
-
-  const handleFormSubmit = async (values, actions) => {
-    setActiveStep(activeStep + 1);
-
-    // this copies the billing address onto shipping address
-    if (isFirstStep && values.shippingAddress.isSameAddress) {
-      actions.setFieldValue("shippingAddress", {
-        ...values.billingAddress,
-        isSameAddress: true,
       });
     }
 
